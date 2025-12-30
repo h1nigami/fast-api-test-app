02c6ed964710b88c2c62970cf6a756acf06b4c19
h1nigami <timokhinivan1@gmail.com>
2025-12-30 14:42:00 +0500

    add: params to swagger

--- api/bookings.py
+++ api/bookings.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from fastapi import status, Depends
+from fastapi import status, Depends, Query
 from schemas import BookingRequest, BookingResponse
 
 from services.booking_service import BookingService
@@ -12,7 +12,12 @@ booking_router = APIRouter(prefix="/api/bookings", tags=["bookings"])
 service = BookingService()
 
 @booking_router.post("/reserve", response_model=BookingResponse, status_code=status.HTTP_201_CREATED)
-async def reserve(request: BookingRequest, db: Session = Depends(get_db)):
+async def reserve(
+                request: BookingRequest,
+                db: Session = Depends(get_db),
+                event_id: int = Query(None, description="ID события"),
+                user_id: str = Query(None, description="ID пользователя")
+                   ):
     """Эндпоинт для бронирования мест"""
     booking = await service.reserve(db=db, event_id=request.event_id, user_id=request.user_id)
     return booking
--- api/events.py
+++ api/events.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from fastapi import status, Depends
+from fastapi import status, Depends, Query
 from schemas import EventRequest, EventResponse
 
 from services.event_service import EventService
@@ -11,7 +11,16 @@ event_router = APIRouter(prefix="/api/events", tags=["events"])
 service = EventService()
 
 @event_router.post("/", response_model=EventResponse, status_code=status.HTTP_201_CREATED)
-async def create_event(request: EventRequest, db: Session = Depends(get_db)):
+async def create_event(
+    request: EventRequest,
+    db: Session = Depends(get_db),
+    description: str = Query(None, description="Описание события"),
+    date: str = Query(None, description="Дата события"),
+):
     """Эндпоинт для создания события"""
-    event = await service.create_event(db=db, name=request.name, total_seats=request.total_seats)
+    event = await service.create_event(
+        db=db,
+        name=request.name,
+        total_seats=request.total_seats,
+    )
     return event
